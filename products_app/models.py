ed2c4c4a584fc097ffe7e1556a940680534269b9
vtymo <vladyslav_tymoshchuk@trimble.com>
2026-06-06 11:52:40 +0300

    Create product list page

--- products_app/models.py
+++ products_app/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    CATEGORY_CHOICES = [
+        ('electronics', 'Electronics'),
+        ('clothing', 'Clothing'),
+        ('books', 'Books'),
+        ('furniture', 'Furniture'),
+    ]
+
+    name = models.CharField(max_length=100)
+    # image = models.ImageField(upload_to='product_images/', null=True)
+    description = models.TextField(blank=True, null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.IntegerField()
+    # category = models.CharField(max_length=50, choices=CATEGORY_CHOICES)
+    discount = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.name} - {self.category}'
--- products_app/views.py
+++ products_app/views.py
@@ -1,9 +1,39 @@
 from django.shortcuts import render
 
+products = [
+    {
+        'name': 'iPhone X',
+        'description': 'Description for product 1',
+        'price': 19.99,
+        'stock': 10,
+        'category': 'electronics',
+        'discount': 10,
+        'image': 'https://content1.rozetka.com.ua/goods/images/big/221203654.jpg',
+    },
+    {
+        'name': 'Samsung Galaxy S21',
+        'description': 'Description for product 2',
+        'price': 29.99,
+        'stock': 5,
+        'category': 'clothing',
+        'discount': 20,
+        'image': 'https://content.rozetka.com.ua/goods/images/big/487792065.jpg',
+    },
+    {
+        'name': 'MacBook Pro',
+        'description': 'Description for product 3',
+        'price': 9.99,
+        'stock': 20,
+        'category': 'books',
+        'discount': 0,
+        'image': 'https://content1.rozetka.com.ua/goods/images/big_tile/655338574.jpg',
+    },
+]
+
 # Create your views here.
 def product_list(request):
     # additional logic (get data from db, validate forms, etc.) can be added here
-    return render(request, 'products/product_list.html')
+    return render(request, 'products/product_list.html', {'products': products})
 
 def about(request):
     return render(request, 'products/about.html')
