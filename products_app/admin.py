76085d456df44005331fef90bff82a7eac433b99
vtymo <vladyslav_tymoshchuk@trimble.com>
2026-07-11 13:06:42 +0300

    Add product model to built-in admin panel

--- products_app/admin.py
+++ products_app/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from products_app.models import Product
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'description')
+    search_fields = ('name', 'description')
+    list_filter = ('price',)
+
 # Register your models here.
+admin.site.register(Product, ProductAdmin)
